73f32463ccb0b2a8c5f2a83b4139b4b036eee49b
Cesar Carbajal <crcarbajal@minsait.com>
2022-09-14 11:42:11 -0500

    Guardando avance

---
+++ src/main/java/com/minsait/junit/models/Cuenta.java
@@ -0,0 +1,54 @@
+package com.minsait.junit.models;
+
+import com.minsait.junit.exceptions.DineroInsuficienteException;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/*@Getter //Actua como el Get para nuestras 3 variables
+@Setter//Actua como el Set para nuestras 3 variables
+@EqualsAndHashCode //Sobreescribir el metodo*/
+
+@Data
+
+public class Cuenta {
+
+    @NonNull
+    private String persona;
+    @NonNull
+    private BigDecimal saldo;
+    private Banco banco;
+
+   /* public Cuenta(String persona, BigDecimal saldo) {
+        this.persona = persona;
+        this.saldo = saldo;
+
+    }*/
+
+    public void retirar(BigDecimal monto){
+        BigDecimal saldoAux = this.saldo.subtract(monto);
+        if (saldoAux.compareTo(BigDecimal.ZERO)<0){
+            throw new DineroInsuficienteException("Dinero Insuficiente");
+
+        }
+        this.saldo = saldoAux;
+
+    }
+    public void depositar(BigDecimal monto){
+        this.saldo =this.saldo.add(monto);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cuenta cuenta = (Cuenta) o;
+        return Objects.equals(persona, cuenta.persona) && Objects.equals(saldo, cuenta.saldo) && Objects.equals(banco, cuenta.banco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(persona, saldo, banco);
+    }
+}
---
+++ src/test/java/com/minsait/junit/models/CuentaTest.java
@@ -0,0 +1,135 @@
+package com.minsait.junit.models;
+
+import com.minsait.junit.exceptions.DineroInsuficienteException;
+import org.junit.jupiter.api.*;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    Cuenta cuenta;
+
+    TestInfo testInfo;
+
+    TestReporter testReporter;
+
+
+    @BeforeEach //se ejecuta despues del before All es el 2do
+    void setUp(TestInfo testInfo, TestReporter testReporter) {
+        this.cuenta = new Cuenta("Bill Gates", new BigDecimal(1000));
+        testReporter.publishEntry("Iniciando el metodo");
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
+        testReporter.publishEntry("Ejecutando: " +testInfo.getTestMethod().orElse(null).getName());
+
+
+    }
+
+    @AfterEach //se ejecuta despues de BeforeEach es el 3ro
+    void tearDown() {
+        System.out.println("Finalizando el metodo de prueba");
+
+    }
+
+    @BeforeAll //es el que se ejecuta antes de todos los metodos, es el 1ro
+    static void beforeAll() {
+        System.out.println("Iniciando todos los test");
+
+    }
+
+    @AfterAll
+    static void afterAll() { //Se ejecuta al final de todos el ciclo de vida, es el 4to
+
+        System.out.println("Finalizando todos los test");
+
+    }
+    @Nested
+
+
+    class CuentaNombreSaldo{
+        @Test
+        void testNombreCuenta() {
+            String esperado = "Cesar";
+            String real = cuenta.getPersona();
+            assertNotNull(real);
+            assertEquals(esperado, real);
+            assertTrue(esperado.equals(real));
+        }
+
+        @Test
+        void testSaldoCuenta() {
+            assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO)<0);
+            assertEquals(1000, cuenta.getSaldo().intValue());
+
+        }
+
+        @Test
+        void testReferencia() {
+            Cuenta cuenta2 = new  Cuenta("Cesar", new BigDecimal(1000));
+            assertNotEquals(cuenta, cuenta2);
+        }
+    }
+
+    @Nested
+    class  CuentaOperacionesTest{
+        @Test
+        void testRetirarCuenta(){
+            cuenta.retirar((new BigDecimal(500)));
+            assertNotNull(cuenta.getSaldo());
+            assertEquals(500, cuenta.getSaldo().doubleValue());
+        }
+        @Test
+        void testDepositarCuenta(){
+            cuenta.depositar((new BigDecimal(500)));
+            assertNotNull(cuenta.getSaldo());
+            assertEquals(1500, cuenta.getSaldo().doubleValue());
+
+        }
+        @Test
+        void testTransferirEntreCuentas(){
+            Cuenta cuenta2 = new Cuenta("Bill Gates", new BigDecimal(10000));
+            Banco banco = new Banco();
+            banco.setNombre("BBVA");
+            banco.tranferir(cuenta2, cuenta, new BigDecimal(9000));
+
+            assertEquals(1000, cuenta2.getSaldo().intValue());
+            assertEquals(10000, cuenta.getSaldo().doubleValue());
+        }
+
+    }
+
+    @Test
+    void testException(){
+        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
+            cuenta.retirar(new BigDecimal(1500));
+        });
+        assertEquals("Dinero Insuficiente", exception.getMessage());
+        //Evalular si la exception es correcta
+        assertEquals(DineroInsuficienteException.class, exception.getClass());
+        
+    }
+
+    @Test
+    @DisplayName("Test de relacion entre cuentas y banco")
+    void testRelacionBancoCuentas() {
+        Cuenta cuenta2 = new Cuenta("Bill Gates", new BigDecimal(10000));
+        Banco banco = new Banco();
+        banco.addCuenta(cuenta2);
+        banco.addCuenta(cuenta);
+        banco.setNombre("BBVA");
+        banco.tranferir(cuenta2, cuenta, new BigDecimal(9000));
+
+        assertAll( //acepta expresiones lamda
+                () -> assertEquals(10000, cuenta.getSaldo().doubleValue()),
+                () -> assertEquals(1000, cuenta2.getSaldo().intValue()),
+                () -> assertEquals(2, banco.getCuentas().size()),
+                () -> assertEquals(banco.getNombre(), cuenta.getBanco().getNombre()),
+                () -> assertEquals(banco.getNombre(), cuenta2.getBanco().getNombre()),
+                () -> assertEquals(cuenta.getPersona(), banco.getCuentas().get(0).getPersona())
+        );
+
+
+    }
+}
